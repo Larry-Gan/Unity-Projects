c78218e0bb4839e8c141b8f26e3f3c230af1beff
Lawrence Gan <lbgan2000@berkeley.edu>
2020-10-17 14:35:40 -0700

    added project

---
+++ RollaBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+
+    private int count;
+
+    private float mvtX;
+    private float mvtY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue mvtValue)
+    {
+        Vector2 mvtVector = mvtValue.Get<Vector2>();
+
+        mvtX = mvtVector.x;
+        mvtY = mvtVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 mvt = new Vector3(mvtX, 0.0f, mvtY);
+
+        rb.AddForce(mvt * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup")) 
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+
+            SetCountText();
+        }
+    }
+}
